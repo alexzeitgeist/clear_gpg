6d5b643db22bdb58f4cc092ccc5f96a533ffd8e2
Alex <alex@zeitgeist.se>
2023-04-11 14:30:55 +0200

    Optimize

--- clear_gpg.go
+++ clear_gpg.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io"
+	"log"
 	"os/exec"
 
 	"github.com/godbus/dbus/v5"
 )
 
+const dbusRule = "type='signal',interface='org.freedesktop.login1.Session',member='Lock'"
+
 func main() {
 	conn, err := dbus.SystemBus()
 	if err != nil {
-		fmt.Println("Error connecting to system bus:", err)
-		return
+		log.Fatal("Error connecting to system bus:", err)
 	}
 
-	rule := "type='signal',interface='org.freedesktop.login1.Session',member='Lock'"
-	if err = conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, rule).Err; err != nil {
-		fmt.Println("Error adding D-Bus match:", err)
-		return
+	if err = conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, dbusRule).Err; err != nil {
+		log.Fatal("Error adding D-Bus match:", err)
 	}
 
 	ch := make(chan *dbus.Signal, 10)
@@ -27,44 +27,32 @@ func main() {
 	for sig := range ch {
 		if sig.Name == "org.freedesktop.login1.Session.Lock" {
 			if err = clearAll(); err != nil {
-				fmt.Println("Error clearing GPG agent:", err)
+				log.Println("Error clearing GPG agent:", err)
 			}
 		}
 	}
 }
 
-func clearAll() (err error) {
-	null, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0)
-	if err != nil {
-		return fmt.Errorf("failed to open /dev/null: %w", err)
-	}
-	defer func() {
-		closeErr := null.Close()
-		if err == nil && closeErr != nil {
-			err = fmt.Errorf("failed to close /dev/null: %w", closeErr)
-		}
-	}()
-
+func clearAll() error {
 	cmds := []struct {
-		name   string
-		args   []string
-		stdout *os.File
+		name string
+		args []string
 	}{
-		{"gpg-connect-agent", []string{"SCD RESET", "/bye"}, null},
-		{"gpg-connect-agent", []string{"reloadagent", "/bye"}, null},
-		{"pkill", []string{"-HUP", "gpg-agent"}, null},
+		{"gpg-connect-agent", []string{"SCD RESET", "/bye"}},
+		{"gpg-connect-agent", []string{"reloadagent", "/bye"}},
+		{"pkill", []string{"-HUP", "gpg-agent"}},
 	}
 
 	for _, c := range cmds {
-		if err = runCommand(c.name, c.args, c.stdout); err != nil {
+		if err := runCommand(c.name, c.args); err != nil {
 			return fmt.Errorf("error running %s: %w", c.name, err)
 		}
 	}
 	return nil
 }
 
-func runCommand(name string, args []string, stdout *os.File) error {
+func runCommand(name string, args []string) error {
 	cmd := exec.Command(name, args...)
-	cmd.Stdout = stdout
+	cmd.Stdout = io.Discard
 	return cmd.Run()
 }
